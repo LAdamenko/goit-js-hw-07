6488015b1970a3578e1dc2b0c70fa0e94b8348f1
LAdamenko <148965340+LAdamenko@users.noreply.github.com>
2024-02-28 11:54:06 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,12 +1,20 @@
 const form = document.querySelector(".login-form");
 form.addEventListener("submit", handleSubmit);
+
 function handleSubmit(event) {
     event.preventDefault();
     const elements = event.currentTarget.elements;
-    const info = {
-    email: elements.email.value,
-    password: elements.password.value,
-    };
-    elements.email.value.trim() === "" || elements.password.value.trim() === "" ? alert("All form fields must be filled in") : console.log(info);
-    event.currentTarget.reset();    
+    const email = elements.email.value.trim();
+    const password = elements.password.value.trim();
+
+    if (email === "" || password === "") {
+        alert("All form fields must be filled in");
+    } else {
+        const info = {
+            email: email,
+            password: password,
+        };
+        console.log(info);
+        event.currentTarget.reset();
+    }    
 }
